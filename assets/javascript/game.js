3a6c8b4853971d503af67ab32a00ab45f231ef40
Derek Reed <1331derek@gmail.com>
2018-09-20 12:32:15 -0600

    bug fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,78 +10,74 @@ var ronaldo = Math.floor(Math.random() * 12) + 1;
 var neymar = Math.floor(Math.random() * 12) + 1;
 
 var updateScore = function () {
-    $("#score").empty();
-    $("#score").append(score);
+    $("#score").text("Current Score: " + score);
 
-    $("#wins").empty();
-    $("#wins").append(wins);
+    $("#wins").text("Wins: " + wins);
 
-    $("#losses").empty();
-    $("#losses").append(losess);
+    $("#losses").text("losess " + losses);
 }
 
-    var restart = function () {
-        score = 0;
-        goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+var restart = function () {
+    score = 0;
+    goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
-        $("#randomscore").empty();
-        $("#randomscore").append(goal);
+    $("#randomscore").text("Goal: " + goal);
 
-        var kane = Math.floor((Math.random() * 12) + 1);
-        var messi = Math.floor((Math.random() * 12) + 1);
-        var ronaldo = Math.floor((Math.random() * 12) + 1);
-        var neymar = Math.floor((Math.random() * 12) + 1);
-        Updatescore();
-    }	
+    var kane = Math.floor((Math.random() * 12) + 1);
+    var messi = Math.floor((Math.random() * 12) + 1);
+    var ronaldo = Math.floor((Math.random() * 12) + 1);
+    var neymar = Math.floor((Math.random() * 12) + 1);
+    updateScore();
+}
 
 var game = function () {
     if (score == goal) {
-        wins = wins + 1;
+        wins++;
         alert("Ahrooooo");
         restart();
     } else if (score > goal) {
-        losses = losses + 1;
+        losses++;
         alert("I'm a little tea pot and that's all I am");
         restart();
     } else {
         updateScore();
     }
 }
-   
-$(document).ready(function() {  
 
-    $("#randomscore").text(goal);
-    
-    $("#messi").click(function() {
+$(document).ready(function () {
+
+    $("#randomscore").text("Goal: " + goal);
+
+    $("#messi").click(function () {
         $("#score").html(score = score + messi);
         game();
     });
 
-    $("#ronaldo").click(function() {
+    $("#ronaldo").click(function () {
         $("#score").html(score = score + ronaldo);
         game();
     });
-    
-    $("#neymar").click(function() {
+
+    $("#neymar").click(function () {
         $("#score").html(score = score + neymar);
         game();
     });
 
-    $("#kane").click(function() {
+    $("#kane").click(function () {
         $("#score").html(score = score + kane);
         game();
     });
 });
-    
+
 // Can't figure out whats going on with losses
 
-    
-    
-   
 
 
 
 
-    
- 
+
+
+
+
+
 
