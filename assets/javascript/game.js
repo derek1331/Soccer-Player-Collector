c3d72bbb62091a2542530eb3ecf54d3b6f226b4d
Derek Reed <1331derek@gmail.com>
2018-09-20 17:53:08 -0600

    prettify and comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,75 @@
-
+// Store wins, losses and score
 var wins = 0;
 var losses = 0;
 var score = 0;
 
+// Apply random numbers to 
 var goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 var kane = Math.floor(Math.random() * 12) + 1;
 var messi = Math.floor(Math.random() * 12) + 1;
 var ronaldo = Math.floor(Math.random() * 12) + 1;
 var neymar = Math.floor(Math.random() * 12) + 1;
 
-var updateScore = function () {
-    $("#score").text("Current Score: " + score);
+// Update wins/loseses and score after game ends
+var updateScore = function() {
+  $("#score").text("Current Score: " + score);
 
-    $("#wins").text("Wins: " + wins);
+  $("#wins").text("Wins: " + wins);
 
-    $("#losses").text("losess " + losses);
-}
+  $("#losses").text("losess " + losses);
+};
 
-var restart = function () {
-    score = 0;
-    goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+// Restart game after it ends
+var restart = function() {
+  score = 0;
+  goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
-    $("#randomscore").text("Goal: " + goal);
+  $("#randomscore").text("Goal: " + goal);
 
-    var kane = Math.floor((Math.random() * 12) + 1);
-    var messi = Math.floor((Math.random() * 12) + 1);
-    var ronaldo = Math.floor((Math.random() * 12) + 1);
-    var neymar = Math.floor((Math.random() * 12) + 1);
+  var kane = Math.floor(Math.random() * 12 + 1);
+  var messi = Math.floor(Math.random() * 12 + 1);
+  var ronaldo = Math.floor(Math.random() * 12 + 1);
+  var neymar = Math.floor(Math.random() * 12 + 1);
+  updateScore();
+};
+
+// Gameplay
+var game = function() {
+  if (score == goal) {
+    wins++;
+    alert("Ahrooooo");
+    restart();
+  } else if (score > goal) {
+    losses++;
+    alert("I'm a little tea pot and that's all I am");
+    restart();
+  } else {
     updateScore();
-}
+  }
+};
 
-var game = function () {
-    if (score == goal) {
-        wins++;
-        alert("Ahrooooo");
-        restart();
-    } else if (score > goal) {
-        losses++;
-        alert("I'm a little tea pot and that's all I am");
-        restart();
-    } else {
-        updateScore();
-    }
-}
+// Applys random numbers to each 
+$(document).ready(function() {
+  $("#randomscore").text("Goal: " + goal);
 
-$(document).ready(function () {
+  $("#messi").click(function() {
+    $("#score").html((score = score + messi));
+    game();
+  });
 
-    $("#randomscore").text("Goal: " + goal);
+  $("#ronaldo").click(function() {
+    $("#score").html((score = score + ronaldo));
+    game();
+  });
 
-    $("#messi").click(function () {
-        $("#score").html(score = score + messi);
-        game();
-    });
+  $("#neymar").click(function() {
+    $("#score").html((score = score + neymar));
+    game();
+  });
 
-    $("#ronaldo").click(function () {
-        $("#score").html(score = score + ronaldo);
-        game();
-    });
-
-    $("#neymar").click(function () {
-        $("#score").html(score = score + neymar);
-        game();
-    });
-
-    $("#kane").click(function () {
-        $("#score").html(score = score + kane);
-        game();
-    });
+  $("#kane").click(function() {
+    $("#score").html((score = score + kane));
+    game();
+  });
 });
-
-// Can't figure out whats going on with losses
-
-
-
-
-
-
-
-
-
-
 
